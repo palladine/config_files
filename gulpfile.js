a45c0c2265d76ec0828b337f3f27339ee01201b4
Maksim <duroff.83@yandex.ru>
2018-01-05 21:24:22 +0700

    v25(css)

--- gulpfile.js
+++ gulpfile.js
@@ -1,56 +1,15 @@
-
-////////////...TASK...Базовый.синтаксис...////////////////////////////////////////////////////////
-///
-///   gulp.task('task', function() {
-///       return gulp.src('file/s')                       ресурс
-///       .pipe(plugin())                                 выполнение плагина над ресурсом
-///       .pipe(gulp.dest('dir'))                         выгрузка в каталог
-///       .pipe(browserSync.reload({stream: true}))       если необходим reload
-///   });
-///
-///   Шаблоны ресурса:
-///   'filename.js'                                       один файл
-///   '[filename1.js, filename2.js, ..., filenameN.js']   массив файлов
-///   'dir/filename.js'                                   один файл из каталога 
-///   'dir/*.js'                                          все файлы с расширением .js из каталога 
-///   '/**/*.js'                                          любая вложенность каталогов
-///   '!filename.js'                                      исключить файл
-///   '*.+(js|css)'                                       все файлы js и css
-///                              
-///////////////////////////////////////////////////////////////////////////////////////////////////
-
-////////////...TASK...Слежение.за.изменением...////////////////////////////////////////////////////
-///
-///   gulp.task('watch', ['browserSync', 'anyTask'], function() {  + таск browserSync
-///       gulp.watch('file/s', ['task/s_name']);                   при изменении file/s выполнить таск
-///       gulp.watch('file/s', browserSync.reload);                при изменении file/s выполнить reload
-///   });
-///
-///////////////////////////////////////////////////////////////////////////////////////////////////
-
-////////////...TASK...browserSync...///////////////////////////////////////////////////////////////
-///
-///   gulp.task('browserSync', function() {
-///       browserSync({
-///           server: {
-///               baseDir: 'dir'
-///           },
-///           notify: true
-///       });
-///   });
-///      
-///////////////////////////////////////////////////////////////////////////////////////////////////
-
-//////  !ДЛЯ LIVERELOAD ТЭГ <body>..</body> ОБЯЗАТЕЛЕН В ФАЙЛЕ! ////////
-
-
-var gulp = require('gulp'), 
+var gulp = require('gulp'),
     php = require('gulp-connect-php'),
     browserSync = require('browser-sync').create();
 
+gulp.task('css', function(){
+    gulp.src('./app/css/*.css')
+    .pipe(browserSync.reload({stream:true}));
+});
+
 gulp.task('sync', function() {
     browserSync.init({
-	proxy: "eduphp.loc"
+	proxy: "eduphp"
     });
 });
 
@@ -60,8 +19,10 @@ gulp.task('connect', function() {
 });
 
 gulp.task('watch', function() {
-    gulp.watch('./public/*.+(php|html|css|js)')
+    gulp.watch('./app/**/*.+(php|html|js)')
     .on('change', function() { browserSync.reload(); });
+    gulp.watch('./app/css/*.css', ['css']);
+    //gulp.watch('./app/css/*.css').on('change', browserSync.reload);
 });
 
 gulp.task('default', ['watch', 'sync', 'connect']);
